a750920a76e53a6be668b64617c5c6197800bf4a
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-05-21 07:57:53 -0500

    Render world border

--- src/main/java/uhcclient/mixin/MixinGameRenderer.java
+++ src/main/java/uhcclient/mixin/MixinGameRenderer.java
@@ -1,9 +1,18 @@
 package uhcclient.mixin;
 
 import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_CULL_FACE;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glColor4f;
+import static org.lwjgl.opengl.GL11.glDisable;
 import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glPopMatrix;
 import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glTranslated;
+import static org.lwjgl.opengl.GL11.glVertex3d;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +20,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.Tessellator;
 import net.minecraft.entity.player.Player;
 import uhcclient.UHCClientMod;
 
@@ -27,24 +35,41 @@ public class MixinGameRenderer {
     )
     private void renderWorldBorder(CallbackInfo ci) {
         Player player = MinecraftAccessor.getInstance().player;
-        int renderDistance = 256 >> MinecraftAccessor.getInstance().options.viewDistance; // Screwy old view distance
-        double distanceToBorder = Math.max(
-            Math.max(
-                UHCClientMod.worldBorder - player.x,
-                player.x + UHCClientMod.worldBorder
-            ),
-            Math.max(
-                UHCClientMod.worldBorder - player.z,
-                player.z + UHCClientMod.worldBorder
-            )
-        );
-        if (distanceToBorder > renderDistance) {
-            return;
-        }
-        // GameRenderer renderer = (GameRenderer)(Object)this;
-        Tessellator tessellator = Tessellator.INSTANCE;
+        double worldBorder = UHCClientMod.worldBorder;
         glPushMatrix();
+        glDisable(GL_TEXTURE_2D);
+        glDisable(GL_CULL_FACE); // We want both sides to render
         glEnable(GL_BLEND);
+        glTranslated(-player.x, -player.y, -player.z);
+        glColor4f(0.1254902f, 0.1254902f, 1.0f, 0.5f);
+        glBegin(GL_QUADS);
+        {
+            glVertex3d(worldBorder, 0,   -worldBorder);
+            glVertex3d(worldBorder, 128, -worldBorder);
+            glVertex3d(worldBorder, 128,  worldBorder);
+            glVertex3d(worldBorder, 0,    worldBorder);
+        }
+        {
+            glVertex3d(-worldBorder, 0,   worldBorder);
+            glVertex3d(-worldBorder, 128, worldBorder);
+            glVertex3d(worldBorder,  128, worldBorder);
+            glVertex3d(worldBorder,  0,   worldBorder);
+        }
+        {
+            glVertex3d(-worldBorder, 0,   -worldBorder);
+            glVertex3d(-worldBorder, 128, -worldBorder);
+            glVertex3d(-worldBorder, 128,  worldBorder);
+            glVertex3d(-worldBorder, 0,    worldBorder);
+        }
+        {
+            glVertex3d(-worldBorder, 0,   -worldBorder);
+            glVertex3d(-worldBorder, 128, -worldBorder);
+            glVertex3d(worldBorder,  128, -worldBorder);
+            glVertex3d(worldBorder,  0,   -worldBorder);
+        }
+        glEnd();
+        glEnable(GL_TEXTURE_2D);
+        glEnable(GL_CULL_FACE);
         glPopMatrix();
     }
 }
